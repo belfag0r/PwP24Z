a518c7ecaf8b528a10815480d5bd239a4d5fc6d2
belfag0r <woy.dec@gmail.com>
2024-12-20 23:04:32 +0100

    Add files via upload

---
+++ 8_1.py
@@ -0,0 +1,30 @@
+# potpowiedź do zadania 8_1
+
+zdanie1 = "To jest zdanie"
+zdanie2 = "To jest kadjasf"
+
+lista_zdan = [zdanie1, zdanie2]
+
+vocab = set(sum([z.split() for z in lista_zdan], []))
+
+print(vocab)
+
+word_2_idx = {word: idx for idx, word in enumerate(vocab)}
+
+print(word_2_idx)
+
+"""
+word_2_df = ...
+word_2_idf = ...
+
+wektory = []
+
+for zdanie in lista_zdan:
+    wektor_zdania = [0] * len(vocab)
+
+    for word in zdanie:
+        tf = zdanie.count(word)/len(zdanie)
+        wektor_zdania[word_2_idx[zdanie]] = tf*idf[word]
+
+    wektory.append(wektor_zdania)
+"""
